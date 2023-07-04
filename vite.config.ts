b06b90b244fa12552441e9a69094ae619835c120
Andrew Rubenstein <andjruben@gmail.com>
2023-07-04 01:26:14 -0400

    chore: setup vite and install necessary packages
    includes workarounds to get taquito working with vite / sveltekit.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,32 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		port: 5173
+	},
+	resolve: {
+		alias: {
+			"readable-stream": "vite-compatible-readable-stream",
+			stream: "vite-compatible-readable-stream"
+		}
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			plugins: [
+				NodeGlobalsPolyfillPlugin({
+					process: true,
+					buffer: true
+				})
+			]
+		},
+	},
+	build: {
+		target: "esnext",
+		commonjsOptions: {
+			transformMixedEsModules: true,
+		},
+	},
 });
